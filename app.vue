7f41286373e18879e26a2c092a8205605571c80a
Unubaatar Bodi-Amar <unuuku_0602@yahoo.com>
2024-10-16 23:07:20 +0800

    f

---
+++ app.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import Header from './Layouts/header.vue';
+</script>
+
+<template>
+  <NuxtLayout>
+  <Header></Header>
+    <NuxtPage />
+  </NuxtLayout>
+</template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
